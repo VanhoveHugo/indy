92e2f8b2dc303da161a538a8842d215401da6f9e
VanhoveHugo <vanhovehugo1@gmail.com>
2023-03-15 22:52:28 +0100

    INDY-6: CRUD Users

---
+++ server/controllers/errors.js
@@ -0,0 +1,15 @@
+// create Errors controller to handle errors and responses to client requests type : Erros.notFound
+
+const Errors = {
+    internalServerError: (res) => {
+        res.status(500).send('Le serveur a rencontré une erreur interne')
+    },
+    pageNotFound: (res) => {
+        res.status(404).send('La page demandée n\'existe pas')
+    },
+    noContent: (res) => {
+        res.status(204).send('La requête a été traitée avec succès mais ne renvoie pas de contenu')
+    }
+}
+
+module.exports = Errors
--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,12 +1,40 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const User = require('../models/users');
+const Errors = require('./errors');
+
+/*
+ *  User Routes
+ */
+router.get('/users', (req, res) => {
+    User.all((users) => {
+        if(!users.some(user => user instanceof User)) return Errors.noContent(res)
+        res.json(users)
+    })
+}).post('/users', (req, res) => {
+    User.create(req.body.firstname, req.body.lastname, req.body.status, req.body.active, (user) => {
+        res.json(user)
+    })
+})
+
+router.get('/users/:id', (req, res) => {
+    User.find(req.params.id, (user) => {
+        if(!user || user.id === null) return Errors.noContent(res)
+        res.json(user)
+    })
+}).put('/users/:id', (req, res) => {
+    User.update(req.params.id, req.body.firstname, req.body.lastname, req.body.status, req.body.active, (user) => {
+        res.json(user)
+    })
+}).delete('/users/:id', (req, res) => {
+    User.delete(req.params.id, (user) => {
+        res.json(user)
+    })
+})
 
 router.get('/*', (req, res) => {
-	res.send('404')
+	Errors.pageNotFound(res)
 })
 
 module.exports = router;
